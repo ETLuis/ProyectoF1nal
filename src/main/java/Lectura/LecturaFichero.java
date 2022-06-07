f11e595fcd916096f9f9e6da77f8fa81749647b5
DaniGonGre <91198318+DaniGonGre@users.noreply.github.com>
2022-06-07 16:41:22 +0200

    Update LecturaFichero.java

--- src/main/java/Lectura/LecturaFichero.java
+++ src/main/java/Lectura/LecturaFichero.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class LecturaFichero {
 
+    // Este método sirve para añadir las palabras sacadas del fichero "palabras.txt" a un arrayList llamado palabras.
     public static void crearPalabra(List<String> palabras){
         File fichero = new File("palabras.txt");
 
